eb9f42fe5e93f2a2fb66ab1d130b93c7e5d9547b
althaf <shaikalthaf1984@gmail.com>
2026-04-24 12:24:37 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0c07985272092a461 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     #to get private ip address
-    if [ $instance != "frontend"]; then
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids i-0863b9cd92831b9e5 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids i-0863b9cd92831b9e5 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
