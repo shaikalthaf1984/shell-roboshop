450696702afcdd63ed84f7ed7d7ae1ef0689256c
althaf <shaikalthaf1984@gmail.com>
2026-04-24 13:13:53 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0c07985272092a461"
+ZONE_ID="Z09234563NW84MGB2OOZ2"
+DOMAIN_NAME="althaf84.org"
 
 for instance in $@
 do
@@ -10,9 +12,30 @@ do
     #to get private ip address
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # out put is mongodb.althaf84.org becuase it is out of backend it takes private ip addres
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" #out put is althaf84.org beacuse it is frontend, it takes public ip address
 
     fi
         echo "$instance: $IP"
+
+        aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
