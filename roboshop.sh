ec4a1bdfbacd85211c52b714aab2201d168dc997
althaf <shaikalthaf1984@gmail.com>
2026-04-24 12:35:59 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,7 @@ do
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
     fi
         echo "$instance: $"
